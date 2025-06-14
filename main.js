ebc3617e2870514581755618df703c4730e60263
SpookyMcGee <netherlainsean@gmail.com>
2025-06-13 21:08:08 -0700

    scroll and gallery
    stops on hover, gallery now side by side

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ function showSection(id) {
 
 // Show default section
 showSection('portraits');
+const carousel = document.querySelector('.carousel');
+
+let scrollAmount = 0;
+const scrollStep = 1;
+const delay = 20; // ms between scroll steps
+let autoScrollInterval;
+
+function startAutoScroll() {
+  autoScrollInterval = setInterval(() => {
+    if (carousel.scrollWidth - carousel.clientWidth <= scrollAmount) {
+      scrollAmount = 0;
+      carousel.scrollTo({ left: 0 });
+    } else {
+      scrollAmount += scrollStep;
+      carousel.scrollBy({ left: scrollStep, behavior: 'smooth' });
+    }
+  }, delay);
+}
+
+function stopAutoScroll() {
+  clearInterval(autoScrollInterval);
+}
+
+carousel.addEventListener('mouseenter', stopAutoScroll);
+carousel.addEventListener('mouseleave', startAutoScroll);
+
+// Start it initially
+startAutoScroll();
