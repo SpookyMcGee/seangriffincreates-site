63b45e42651b96030d6a24037aa2e586f4947d37
SpookyMcGee <netherlainsean@gmail.com>
2025-06-19 13:13:53 -0700

    added script

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FloatingBottomNav from './FloatingBottomNav';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <FloatingBottomNav />
+  </React.StrictMode>
+);
