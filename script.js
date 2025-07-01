3c335d2d4839c823897ce5158a0e100b70ea9272
SpookyMcGee <netherlainsean@gmail.com>
2025-07-01 11:21:11 -0700

    portraits page and filenames

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
         navUl.classList.toggle('active');
     });
 });
+// script.js
+function openLightbox(src) {
+  const lightbox = document.getElementById('lightbox');
+  const img = document.getElementById('lightbox-img');
+  img.src = src;
+  lightbox.classList.remove('hidden');
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').classList.add('hidden');
+}
